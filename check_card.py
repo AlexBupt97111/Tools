309b554051a0a91b10ea1c3200e49645c502ca45
AlexBupt97111 <95982030+AlexBupt97111@users.noreply.github.com>
2022-11-07 10:44:44 +0200

    Update check_card.py

--- check_card.py
+++ check_card.py
@@ -1,11 +1,14 @@
-num = int(input("Input your credit card number, please: "))
-x = [int(a) for a in str(num)]
-if len(x) < 16:
-    print("Too short. Are you input all digits? Check please.")
-elif len(x) > 16:
-  print("Too long. Somewhat redundant. Check please.")
-else:
-    c = x[-4:] #last 4 digits
-    b = ''.join(str(a) for a in c) #list to str
-    print("************",b)
-    print("Ok. Choose next action, please")
+try:
+    num = int(input("Input your credit card number, please: "))
+    x = [int(a) for a in str(num)]
+    if len(x) < 16:
+        print("Ups. Are you input all digits? Check please.")
+    elif len(x) > 16:
+        print("Too long. Somewhat redundant. Check please.")
+    else:
+        c = x[-4:] 
+        b = ''.join(str(a) for a in c) 
+        print("************",b)
+        print("All ok.")    
+except ValueError:
+    print("Some mistake. Input only digits, not letters or other symbols. Check please, and try it again.")  
