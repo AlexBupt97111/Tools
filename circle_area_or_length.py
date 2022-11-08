5b2236509ce9d9739d72d41d6ed87f9022af92db
AlexBupt97111 <95982030+AlexBupt97111@users.noreply.github.com>
2022-11-08 16:22:24 +0200

    Update circle_area_or_length.py

--- circle_area_or_length.py
+++ circle_area_or_length.py
@@ -1,44 +1,25 @@
 import math
-r = float(input("Input radius of circle: "))
-
-def getLength(r):
-    l = 2 * math.pi * r
-    print("Length of circle is", round(l, 2), "cm.")
-    
-def getArea(r):
-    a = math.pi * (r**2)
-    print("Area of circle is", round(a, 2), "sq.cm.")
-
-b = input("Input L(for length) or A(for area): ")
-if b == "L":
-    getLength(r)
-elif b == "A":
-    getArea(r)   
-else:
-    print("Mistake. Input L or A. ")
-    
-###############
-
-import math
-
-class Circle:
-    r = float(input("Input radius of circle: "))
-    b = input("Input L(for length) or A(for area): ")
+try:
+    class Circle:
+        r = float(input("Input radius of circle: "))
+        b = input("Input L(for length) or A(for area): ")
   
-    def getLength(r):
-        l = 2 * math.pi * r
-        print("Length of circle is", round(l, 2), "cm.") 
+        def getLength(r):
+            l = 2 * math.pi * r
+            print("Length of circle is", round(l, 2), "cm.") 
      
-    def getArea(r):
-        a = math.pi * (r**2)
-        print("Area of circle is", round(a, 2), "sq.cm.")
+        def getArea(r):
+            a = math.pi * (r**2)
+            print("Area of circle is", round(a, 2), "sq.cm.")
+            
+        if b == "L":
+            getLength(r)
+        elif b == "A":
+            getArea(r)   
+        else:
+            print("Mistake. Input L or A. ")  
+            
+    c = Circle()    
+except ValueError:
+    print("Input only digits, not letters or other symbols.Check please and try it again")
 
-      
-    if b == "L":
-        getLength(r)
-    elif b == "A":
-        getArea(r)   
-    else:
-        print("Mistake. Input L or A. ")
-      
-c = Circle()
