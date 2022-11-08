8f9aceeb340b953fc30425a4ca4308eec1cdd995
AlexBupt97111 <95982030+AlexBupt97111@users.noreply.github.com>
2022-11-08 15:14:06 +0200

    Update circle_area_or_length.py

--- circle_area_or_length.py
+++ circle_area_or_length.py
@@ -1,18 +1,18 @@
 import math
 r = float(input("Input radius of circle: "))
 
+def getLength(r):
+    p = 2 * math.pi * r
+    print("Length of circle is", round(p, 2), "cm.")
+    
 def getArea(r):
     a = math.pi * (r**2)
     print("Area of circle is", round(a, 2), "sq.cm.")
 
-def getLength(r):
-    p = 2 * math.pi * r
-    print("Length of circle is", round(p, 2), "cm.")
-
-a = input("Input P(for perimeter) or A(for area): ")
-if a == "P":
+a = input("Input L(for length) or A(for area): ")
+if a == "L":
     getLength(r)
 elif a == "A":
     getArea(r)   
 else:
-    print("Mistake. Input P or A. ")
+    print("Mistake. Input L or A. ")
